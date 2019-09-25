0c13204cc792dbd9ea93c542ed638c624b46d6ec
n33alawar <nourhamza1987@gmail.com>
2019-09-25 10:18:56 +0200

    adding source dir

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+export const Header = props => {
+ return (
+   <header className="header">
+     <h1>what to do?</h1>
+     <span className="tagline">{props.tagline}</span>
+   </header>
+ );
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@fortawesome/fontawesome-free/css/all.css';
+import { Router } from './components/Router';
+ReactDOM.render(<Router />, document.querySelector('#root'));
